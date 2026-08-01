f10527e4f2941d6b0c10e4e791749c35f2594cde
Fazeen Nadeem <zeeni1871@gmail.com>
2026-08-01 07:43:32 -0700

    Added security and AI analysis report

--- main.py2.py
+++ main.py2.py
@@ -1,6 +1,6 @@
-print("=== AI Phishing Email Detector ===")
+print("========== AI Phishing Email Detector ==========")
 
-email = input("Paste the email here:\n")
+email = input("Please enter the email content to analyze:\n")
 
 keywords = [
     "urgent",
@@ -16,17 +16,47 @@ keywords = [
 ]
 
 count = 0
+link_found = False
+exclamation_found = False
+reasons=[]
 
+print("\n----- Scanning Email -----")
+
+# Check suspicious keywords
 for word in keywords:
     if word.lower() in email.lower():
         print(f"⚠ Suspicious keyword found: {word}")
+        reasons.append(f"Suspicious keyword: {word}")
         count += 1
 
-print("\nTotal suspicious keywords:", count)
+# Check suspicious links
+if "http://" in email or "https://" in email or "bit.ly" in email or "tinyurl" in email:
+    print("⚠ Suspicious link detected!")
+    link_found = True
+    reasons.append("Contains a suspicious link")
 
-if count == 0:
-    print("🟢 Risk Level: LOW")
-elif count <= 3:
-    print("🟡 Risk Level: MEDIUM")
+# Check too many exclamation marks
+if email.count("!") >= 3:
+    print("⚠ Too many exclamation marks detected!")
+    exclamation_found = True
+    reasons.append("Uses too many exclamation marks")
+print("\n========== SECURITY REPORT ==========")
+
+
+if count >= 5 or (link_found and count >= 2):
+    print("\n🔴 Risk Level: HIGH")
+elif count >= 2:
+    print("\n🟡 Risk Level: MEDIUM")
 else:
-    print("🔴 Risk Level: HIGH")
+    print("\n🟢 Risk Level: LOW")
+
+print("====================================")
+print("\n========== AI ANALYSIS REPORT ==========")
+
+if reasons:
+    for reason in reasons:
+        print("•", reason)
+else:
+    print("No suspicious indicators found.")
+
+print("========================================")
